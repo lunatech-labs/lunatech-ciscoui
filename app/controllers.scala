e75c336cf71ca38c5ebabe8878888b42762a65c7
Sietse de Kaper <sietse+git@lunatech.com>
2011-10-21 12:57:53 +0200

    Simple search working

--- app/controllers.scala
+++ app/controllers.scala
@@ -7,30 +7,45 @@ import org.apache.directory.shared.ldap.model.message._
 import org.apache.directory.ldap.client.api.{LdapConnection, LdapNetworkConnection}
 import collection.JavaConverters._
 import org.apache.directory.shared.ldap.model.cursor.EntryCursor
+import org.apache.directory.shared.ldap.model.entry.Entry
 
 
 object Application extends Controller {
 
   def index = Template("Application/index.xml", ('baseUrl -> configuration("application.baseUrl")))
 
-  def search = {
-    val res: EntryCursor = searchWithLdapConnection(conn => conn.search(configuration("ldap.baseDn"), "(objectclass=*)", SearchScope.ONELEVEL, "*"))
-
-    for (result <- res.asScala)
-      Logger.warn("%s", result)
-
-    <foo/>
+  private val searchAll = {
+    connection :LdapConnection =>
+      for (res <- connection.search(configuration("ldap.baseDn"), "(&(objectclass=person)(cn=sietse*))", SearchScope.ONELEVEL, "*").asScala)
+      yield res
   }
 
-  def searchWithLdapConnection(operation: LdapConnection => EntryCursor): EntryCursor = {
-    val conn: LdapNetworkConnection = new LdapNetworkConnection(configuration("ldap.host"), configuration("ldap.port").toInt)
+  def search = {
+    val searchResults = searchWithLdapConnection(searchAll)
+<CiscoIPPhoneMenu>
+  <Title>Search results</Title>
+  <Prompt>Found {searchResults.size} entries</Prompt>
+  {
+    for(result <- searchResults)
+      yield
+      <MenuItem>
+        <Name>{result.get("displayName").get().getValue()}</Name>
+        <URL>{configuration("application.baseUrl")}entries/{result.getDn}</URL>
+    </MenuItem>
+  }
+</CiscoIPPhoneMenu>
+
+
+  }
+
+  def searchWithLdapConnection[T](operation: LdapConnection => T) :T  = {
+    val connection: LdapNetworkConnection = new LdapNetworkConnection(configuration("ldap.host"), configuration("ldap.port").toInt)
     try {
-      conn.bind(configuration("ldap.user"), configuration("ldap.password"))
-      val res: EntryCursor = conn.search(configuration("ldap.baseDn"), "(objectclass=*)", SearchScope.ONELEVEL, "*")
-      res
+      connection.bind(configuration("ldap.user"), configuration("ldap.password"))
+      operation(connection)
     } finally {
-      conn.unBind()
-      conn.close()
+      connection.unBind()
+      connection.close()
     }
   }
 
